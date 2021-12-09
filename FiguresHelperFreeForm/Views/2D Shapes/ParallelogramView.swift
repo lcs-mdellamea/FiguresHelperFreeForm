62b1732e41f1933e851ff26ce8559f937b443858
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-08 20:08:20 -0500

    Was able to remove slider and implement the TextField for ParallelogramsView! I did this using Computed properties using a Guard Let Statement and I used Ternary Conditional Operators to make the foreground of the text red if user input is not usable.

--- FiguresHelperFreeForm/Views/2D Shapes/ParallelogramView.swift
+++ FiguresHelperFreeForm/Views/2D Shapes/ParallelogramView.swift
@@ -10,13 +10,94 @@ import SwiftUI
 struct ParallelogramView: View {
     
     // MARK: Stored properties
-    @State var b = 10.0
-    @State var h = 10.0
-    @State var c = 10.0
+    @State var providedBase = ""
+    @State var providedHeight = ""
+    @State var providedDiagonal = ""
 
     // MARK: Computed properties
-    var area: Double {
-        return b * h
+    
+    var Base: Double? {
+        // Tests of the provided input
+        // 1. Ensure that we can simply change the input into a double
+        // 2. Ensure that the value as a double is more than 0
+        
+        // With a guard statement, we list the things we want to be true, and provide an action to carry out when those conditions are not met.
+        
+        guard let Base = Double(providedBase),
+              Base > 0
+        else {
+            // When the tests are failed, we do not have a valid radius
+            return nil
+            
+        }
+        
+        // If we get here, we know the radius is good
+        return Base
+        
+    }
+    
+    var Height: Double? {
+        // Tests of the provided input
+        // 1. Ensure that we can simply change the input into a double
+        // 2. Ensure that the value as a double is more than 0
+        
+        // With a guard statement, we list the things we want to be true, and provide an action to carry out when those conditions are not met.
+        
+        guard let Height = Double(providedHeight),
+              Height > 0
+        else {
+            // When the tests are failed, we do not have a valid radius
+            return nil
+            
+        }
+        
+        // If we get here, we know the radius is good
+        return Height
+        
+    }
+    
+    var Diagonal: Double? {
+        // Tests of the provided input
+        // 1. Ensure that we can simply change the input into a double
+        // 2. Ensure that the value as a double is more than 0
+        
+        // With a guard statement, we list the things we want to be true, and provide an action to carry out when those conditions are not met.
+        
+        guard let Diagonal = Double(providedDiagonal),
+              Diagonal > 0
+        else {
+            // When the tests are failed, we do not have a valid radius
+            return nil
+            
+        }
+        
+        // If we get here, we know the radius is good
+        return Diagonal
+        
+    }
+    
+    var area: Double? {
+        
+        guard let Base = Base else {
+            // We didn't have a valid radius
+            // We can't calculate the area...
+            return nil
+        }
+        
+        guard let Height = Height else {
+            // We didn't have a valid radius
+            // We can't calculate the area...
+            return nil
+        }
+        
+        guard let Diagonal = Diagonal else {
+            // We didn't have a valid radius
+            // We can't calculate the area...
+            return nil
+        }
+        
+        
+        return Base * Height
     }
     
     var body: some View {
@@ -34,69 +115,49 @@ struct ParallelogramView: View {
                     SectionLabelView(text: "Base", variable: "b")
 
                     // Input: Base
-                    Slider(value: $b,
-                           in: 0.0...100.0,
-                           step: 0.1,
-                           label: {
-                        Text("Base")
-                    },
-                           minimumValueLabel: {
-                        Text("0")
-                    },
-                           maximumValueLabel: {
-                        Text("100")
-                    })
+                    
+                    TextField("Radius", text: $providedBase, prompt: Text("Numeric value greater than 0"))
+                    //                           Ternary conditional operators
+                    //                             "One line if statement"
+                    //                           CONDITION      TRUE        FALSE
+                        .foregroundColor(Base == nil ? Color.red : Color.primary)
                     
                     // Output: Base
-                    SliderValueView(value: b)
 
                 }
                 
                 // Height
+                
                 Group {
                     
                     SectionLabelView(text: "Height", variable: "h")
 
                     // Input: Height
-                    Slider(value: $h,
-                           in: 0.0...100.0,
-                           step: 0.1,
-                           label: {
-                        Text("Height")
-                    },
-                           minimumValueLabel: {
-                        Text("0")
-                    },
-                           maximumValueLabel: {
-                        Text("100")
-                    })
+                    
+                    TextField("Radius", text: $providedHeight, prompt: Text("Numeric value greater than 0"))
+                    //                           Ternary conditional operators
+                    //                             "One line if statement"
+                    //                           CONDITION      TRUE        FALSE
+                        .foregroundColor(Height == nil ? Color.red : Color.primary)
                     
                     // Output: Base
-                    SliderValueView(value: h)
-
                 }
 
                 // Diagonal
+                
                 Group {
                     
                     SectionLabelView(text: "Diagonal", variable: "c")
 
                     // Input: Height
-                    Slider(value: $c,
-                           in: 0.0...100.0,
-                           step: 0.1,
-                           label: {
-                        Text("Height")
-                    },
-                           minimumValueLabel: {
-                        Text("0")
-                    },
-                           maximumValueLabel: {
-                        Text("100")
-                    })
+                    
+                    TextField("Radius", text: $providedDiagonal, prompt: Text("Numeric value greater than 0"))
+                    //                           Ternary conditional operators
+                    //                             "One line if statement"
+                    //                           CONDITION      TRUE        FALSE
+                        .foregroundColor(Diagonal == nil ? Color.red : Color.primary)
                     
                     // Output: Base
-                    SliderValueView(value: c)
 
                 }
 
